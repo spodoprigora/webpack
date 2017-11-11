488b0578372d28c7a392f34d8ca7f4c2bd72c2e3
Sergey <slivers@ukr.net>
2017-11-11 18:12:47 +0200

    pug done

--- build/blog.js
+++ build/blog.js
@@ -60,22 +60,11 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__menu__ = __webpack_require__(1);
-
-const menu = Object(__WEBPACK_IMPORTED_MODULE_0__menu__["a" /* default */])(['Главная', 'Обо мне', 'Портфолио'], 'menu');
-document.body.appendChild(menu)
-
-/***/ }),
-/* 1 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -89,6 +78,20 @@ document.body.appendChild(menu)
     menu.innerHTML = listItems;
     return menu;
 });
+
+/***/ }),
+/* 1 */,
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__components_menu_menu__ = __webpack_require__(0);
+
+const menu = Object(__WEBPACK_IMPORTED_MODULE_0__components_menu_menu__["a" /* default */])(['Главная','Блог'], 'menu');
+document.body.appendChild(menu);
+
+console.log('in blog.js')
 
 /***/ })
 /******/ ]);
--- source/index.js
+++
@@ -1,3 +0,0 @@
-import createMenu from './menu';
-const menu = createMenu(['Главная', 'Обо мне', 'Портфолио'], 'menu');
-document.body.appendChild(menu)
