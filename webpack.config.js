2c2943c705868abb4865ed7f9c625e4ab3683c68
Sergey <slivers@ukr.net>
2017-11-12 15:21:15 +0200

    separated common js and css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const pug = require('./webpack/pug');
@@ -25,14 +26,17 @@ const common = merge([
         plugins: [
             new HtmlWebpackPlugin({
                 filename: 'index.html',
-                chunks: ['index'],
+                chunks: ['index', 'common'],
                 template: PATHS.source + '/pages/index/index.pug',
             }),
             new HtmlWebpackPlugin({
                 filename: 'blog.html',
-                chunks: ['blog'],
+                chunks: ['blog', 'common'],
                 template: PATHS.source + '/pages/blog/blog.pug',
             }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'common'
+            })
         ]
     },
     pug()
